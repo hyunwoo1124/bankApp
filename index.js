b6f00e359082738e6a6c2e933939beccaff87994
Nitsua22 <austint22@csu.fullerton.edu>
2020-05-18 02:32:00 -0700

    Should be all working but needs some Quality adjustments

--- index.js
+++ index.js
@@ -86,8 +86,9 @@ app.post('/create', function(req, res){
     let password = req.body.password;
     let address = req.body.address;
     let accountBalance = 0;
+    let extraBalance = -5;
 
-    let query = "USE users; INSERT INTO appusers VALUES('" + firstname + "','" + lastname + "','" + username + "','" + password + "','" + address + "','" + accountBalance + "');";
+    let query = "USE users; INSERT INTO appusers VALUES('" + firstname + "','" + lastname + "','" + username + "','" + password + "','" + address + "','" + accountBalance + "','" + extraBalance + "','" + extraBalance + "');";
     console.log(query);
 
     mysqlConn.query(query, function(err, qResult){
@@ -100,54 +101,55 @@ function replaceAll(src, search, replacement) {
     return src.replace(new RegExp(search, 'g'), replacement);
 };
 
+app.post('/balanceRedirect', function(req,res){
+    res.sendFile(__dirname + '/balance.html');
+});
+
 app.post('/balance', function(req,res){
-    //let username = req.body.username;
-    //let password = req.body.password;
-    //let  accountBalance = 0;
-
-    //let query = "USE users; SELECT username, password, accountBalance from appusers where username='" + username + "' AND password='" + password + "' AND accountBalance='" + accountBalance +"');";
-    console.log(req.session.username);
-
     let username = req.session.username;
-    let accountBalance;
+    let accountNum = req.body.accountNum;
+    let accountBalance = -5;
 
     console.log(username);
-    let query = "USE users;SELECT accountBalance FROM appusers WHERE username= '" + username +"';";
-    console.log("pay attention here");
-    console.log(query);
-    console.log(accountBalance);
-    console.log(req.session.accountBalance);
+    let query = "USE users;SELECT accountBalance"+ accountNum +" FROM appusers WHERE username= '" + username +"';";
     mysqlConn.query(query, function(err, qResult){
         if(err) throw err;
         else{
         console.log("1. Check");
         console.log(qResult[1]);
+        let thisAccount = "accountBalance" + accountNum;
         qResult[1].forEach(function(account){
-            accountBalance = account['accountBalance'];
+            accountBalance = account[thisAccount];
             //console.log(accountBalance);
-            req.session.accountBalance = accountBalance;
-            let commentsData = "";
-
-            // Replace the newlines with HTML <br>
-            commentsData = replaceAll(commentsData, "\n", "<br>");
-            
-            let pageStr = "	<!DOCTYPE html>";
-            pageStr += "	<html>";
-            pageStr += "	<head>";
-            pageStr += "		<title>Balance </title>";
-            pageStr += "	</head>";
-            pageStr += "	<body bgcolor=white>";
-            pageStr += "	   <h1>Your current Balance: " + accountBalance + "</h1><br>";
-            pageStr += commentsData;
-            pageStr += "	    <form action='/return' method='post'>";
-            pageStr += "        	    <label for='comment'>Message:</label>"; 
-            pageStr += "        	    <input type='submit' value='Return' />";
-            pageStr += "	    </form>";
-            pageStr += "	</body>";
-            pageStr += "</html>	";
+            if (accountBalance < 0){
+                console.log("got to accountNotReal");
                 
-            // Send the page
-            res.send(pageStr);	
+                res.sendFile(__dirname + '/noAccount.html');	
+            }
+            else{
+                let commentsData = "";
+
+                // Replace the newlines with HTML <br>
+                commentsData = replaceAll(commentsData, "\n", "<br>");
+                
+                let pageStr = "	<!DOCTYPE html>";
+                pageStr += "	<html>";
+                pageStr += "	<head>";
+                pageStr += "		<title>Balance </title>";
+                pageStr += "	</head>";
+                pageStr += "	<body bgcolor=white>";
+                pageStr += "	   <h1>Your current Balance in account " + accountNum + ": " + accountBalance + "</h1><br>";
+                pageStr += commentsData;
+                pageStr += "	    <form action='/return' method='post'>";
+                pageStr += "        	    <label for='comment'>Message:</label>"; 
+                pageStr += "        	    <input type='submit' value='Return' />";
+                pageStr += "	    </form>";
+                pageStr += "	</body>";
+                pageStr += "</html>	";
+                    
+                // Send the page
+                res.send(pageStr);
+            }	
         });
         }
         
@@ -166,19 +168,42 @@ app.post('/deposit', function(req,res){
     let username = req.session.username;
     console.log(username);
     let amountAdd = req.body.deposit;
-    if (amountAdd < 0){
-        res.sendFile(__dirname + '/deposit.html');
-    }
-    else{
-        let query = "USE users; UPDATE appusers SET accountBalance = accountBalance + " + amountAdd + " WHERE username = '" + username + "';";
+    let accountNum = req.body.accountNum;
+    let accountBalance = -5;
 
-        mysqlConn.query(query, function(err, qResult){
-            if(err) throw err;
-        
+    console.log(username);
+    let query1 = "USE users;SELECT accountBalance"+ accountNum +" FROM appusers WHERE username= '" + username +"';";
+    mysqlConn.query(query1, function(err, qResult){
+        if(err) throw err;
+        else{
+        let thisAccount = "accountBalance" + accountNum;
+        qResult[1].forEach(function(account){
+            accountBalance = account[thisAccount];
+            //console.log(accountBalance);
+            if (accountBalance < 0){
+                console.log("got to accountNotReal");
+                
+                res.sendFile(__dirname + '/noAccount.html');	
+            }
+            else{
+                if (amountAdd < 0){
+                    res.sendFile(__dirname + '/deposit.html');
+                }
+                else{
+                    let query2 = "USE users; UPDATE appusers SET accountBalance" + accountNum + " = accountBalance" + accountNum + " + " + amountAdd + " WHERE username = '" + username + "';";
+            
+                    mysqlConn.query(query2, function(err, qResult){
+                        if(err) throw err;
+                    
+                    });
+            
+                   res.sendFile(__dirname + '/dashboard.html');
+                }
+            }	
         });
-
-       res.sendFile(__dirname + '/dashboard.html');
-    }
+        }
+        
+    });
 });
 
 app.post('/withdrawRedirect', function(req,res){
@@ -189,20 +214,271 @@ app.post('/withdraw', function(req,res){
     let username = req.session.username;
     console.log(username);
     let amountSub = req.body.withdraw;
-    if (amountSub < 0){
-        res.sendFile(__dirname + '/withdraw.html');
-    }
-    else{
-        let query = "USE users; UPDATE appusers SET accountBalance = accountBalance - " + amountSub + " WHERE username = '" + username + "';";
+    let accountNum = req.body.accountNum;
+    let accountBalance = -5;
 
-        mysqlConn.query(query, function(err, qResult){
-            if(err) throw err;
-        
+    let query1 = "USE users;SELECT accountBalance"+ accountNum +" FROM appusers WHERE username= '" + username +"';";
+    mysqlConn.query(query1, function(err, qResult){
+        if(err) throw err;
+        else{
+        let thisAccount = "accountBalance" + accountNum;
+        qResult[1].forEach(function(account){
+            accountBalance = account[thisAccount];
+            //console.log(accountBalance);
+            if (accountBalance < 0){
+                console.log("got to accountNotReal");
+                
+                res.sendFile(__dirname + '/noAccount.html');	
+            }
+            else{
+                if ((amountSub < 0) || (amountSub > accountBalance)){
+                    res.sendFile(__dirname + '/withdraw.html');
+                }
+                else{
+                    let query2 = "USE users; UPDATE appusers SET accountBalance" + accountNum + " = accountBalance" + accountNum + " - " + amountSub + " WHERE username = '" + username + "';";
+            
+                    mysqlConn.query(query2, function(err, qResult){
+                        if(err) throw err;
+                    
+                    });
+            
+                   res.sendFile(__dirname + '/dashboard.html');
+                }
+            }	
         });
+        }
+        
+    });
 
-       res.sendFile(__dirname + '/dashboard.html');
+});
+
+app.post('/createAccountRedirect', function(req,res){
+    res.sendFile(__dirname + '/createAccount.html');
+});
+
+app.post('/createAccount', function(req,res){
+    let username = req.session.username;
+    let accountNum = req.body.accountNum;
+    let accountBalance = -5;
+    let accountSetZero = 0;
+
+    console.log(username);
+    let query = "USE users;SELECT accountBalance"+ accountNum +" FROM appusers WHERE username= '" + username +"';";
+    mysqlConn.query(query, function(err, qResult){
+        if(err) throw err;
+        else{
+        console.log("1. Check");
+        console.log(qResult[1]);
+        let thisAccount = "accountBalance" + accountNum;
+        qResult[1].forEach(function(account){
+            accountBalance = account[thisAccount];
+            //console.log(accountBalance);
+            if (accountBalance < 0){
+                console.log("got to exists");
+                let query2 = "USE users; UPDATE appusers SET accountBalance" + accountNum + " = " + accountSetZero + " WHERE username = '" + username + "';";
+                mysqlConn.query(query2, function(err, qResult){
+                    if(err) throw err;
+                });
+                let pageStr = "	<!DOCTYPE html>";
+                pageStr += "	<html>";
+                pageStr += "	<head>";
+                pageStr += "		<title>Account Creation </title>";
+                pageStr += "	</head>";
+                pageStr += "	<body bgcolor=white>";
+                pageStr += "	   <h1>Your account " + accountNum + " is now created</h1><br>";
+                pageStr += "	    <form action='/return' method='post'>";
+                pageStr += "        	    <label for='comment'>Return to dashboard:</label>"; 
+                pageStr += "        	    <input type='submit' value='Return' />";
+                pageStr += "	    </form>";
+                pageStr += "	</body>";
+                pageStr += "</html>	";
+                    
+                // Send the page
+                res.send(pageStr);	
+            }
+            else{
+                
+                let pageStr = "	<!DOCTYPE html>";
+                pageStr += "	<html>";
+                pageStr += "	<head>";
+                pageStr += "		<title>Account Creation </title>";
+                pageStr += "	</head>";
+                pageStr += "	<body bgcolor=white>";
+                pageStr += "	   <h1>Your account " + accountNum + " already exists, try another</h1><br>";
+                pageStr += "	    <form action='/return' method='post'>";
+                pageStr += "        	    <label for='comment'>Return to dashboard:</label>"; 
+                pageStr += "        	    <input type='submit' value='Return' />";
+                pageStr += "	    </form>";
+                pageStr += "	</body>";
+                pageStr += "</html>	";
+                    
+                // Send the page
+                res.send(pageStr);
+            }	
+        });
+        } 
+    });
+});
+
+app.post('/deleteAccountRedirect', function(req,res){
+    res.sendFile(__dirname + '/deleteAccount.html');
+});
+
+app.post('/deleteAccount', function(req,res){
+    let username = req.session.username;
+    let accountNum = req.body.accountNum;
+    let accountBalance = -5;
+    let accountSetNeg = -5;
+
+    console.log(username);
+    let query = "USE users;SELECT accountBalance"+ accountNum +" FROM appusers WHERE username= '" + username +"';";
+    mysqlConn.query(query, function(err, qResult){
+        if(err) throw err;
+        else{
+        console.log("1. Check");
+        console.log(qResult[1]);
+        let thisAccount = "accountBalance" + accountNum;
+        qResult[1].forEach(function(account){
+            accountBalance = account[thisAccount];
+            //console.log(accountBalance);
+            if (accountBalance >= 0){
+                console.log("got to exists");
+                let query2 = "USE users; UPDATE appusers SET accountBalance" + accountNum + " = " + accountSetNeg + " WHERE username = '" + username + "';";
+                mysqlConn.query(query2, function(err, qResult){
+                    if(err) throw err;
+                });
+                let pageStr = "	<!DOCTYPE html>";
+                pageStr += "	<html>";
+                pageStr += "	<head>";
+                pageStr += "		<title>Account Deletion </title>";
+                pageStr += "	</head>";
+                pageStr += "	<body bgcolor=white>";
+                pageStr += "	   <h1>Your account " + accountNum + " is now deleted</h1><br>";
+                pageStr += "	    <form action='/return' method='post'>";
+                pageStr += "        	    <label for='comment'>Return to dashboard:</label>"; 
+                pageStr += "        	    <input type='submit' value='Return' />";
+                pageStr += "	    </form>";
+                pageStr += "	</body>";
+                pageStr += "</html>	";
+                    
+                // Send the page
+                res.send(pageStr);	
+            }
+            else{
+                
+                let pageStr = "	<!DOCTYPE html>";
+                pageStr += "	<html>";
+                pageStr += "	<head>";
+                pageStr += "		<title>Account Deletion </title>";
+                pageStr += "	</head>";
+                pageStr += "	<body bgcolor=white>";
+                pageStr += "	   <h1>Your account " + accountNum + " doesn't exist yet, try another</h1><br>";
+                pageStr += "	    <form action='/return' method='post'>";
+                pageStr += "        	    <label for='comment'>Return to dashboard:</label>"; 
+                pageStr += "        	    <input type='submit' value='Return' />";
+                pageStr += "	    </form>";
+                pageStr += "	</body>";
+                pageStr += "</html>	";
+                    
+                // Send the page
+                res.send(pageStr);
+            }	
+        });
+        } 
+    });
+});
+
+app.post('/transferRedirect', function(req,res){
+    res.sendFile(__dirname + '/transfer.html');
+});
+app.post('/transfer', function(req,res){
+    let username = req.session.username;
+    let amountTransfer = req.body.transferAmount;
+    let transferAccount1 = req.body.accountNum1;
+    let transferAccount2 = req.body.accountNum2;
+    let accountBalance1 = -5;
+    let accountBalance2 = -5;
+
+    if(transferAccount1 == transferAccount2) {
+        let pageStr = "	<!DOCTYPE html>";
+                pageStr += "	<html>";
+                pageStr += "	<head>";
+                pageStr += "		<title>Account Goof </title>";
+                pageStr += "	</head>";
+                pageStr += "	<body bgcolor=white>";
+                pageStr += "	   <h1>You cannot use the same account for both! Try again</h1><br>";
+                pageStr += "	    <form action='/return' method='post'>";
+                pageStr += "        	    <label for='comment'>Return to dashboard:</label>"; 
+                pageStr += "        	    <input type='submit' value='Return' />";
+                pageStr += "	    </form>";
+                pageStr += "	</body>";
+                pageStr += "</html>	";
+                    
+                // Send the page
+                res.send(pageStr);
+    }
+    else {
+        let query1 = "USE users;SELECT accountBalance"+ transferAccount1 +" FROM appusers WHERE username= '" + username +"';";
+        mysqlConn.query(query1, function(err, qResult1){
+            if(err) throw err;
+            else{
+            let thisAccount1 = "accountBalance" + transferAccount1;
+            qResult1[1].forEach(function(account){
+                accountBalance1 = account[thisAccount1];
+                //console.log(accountBalance);
+                if (accountBalance1 < 0){
+                    console.log("got to accountNotReal");    
+                    res.sendFile(__dirname + '/noAccount.html');	
+                }
+                else{
+                    if ((amountTransfer < 0) || (amountTransfer > accountBalance1)){
+                        res.sendFile(__dirname + '/transfer.html');
+                    }
+                    else{
+                        let query2 = "USE users;SELECT accountBalance"+ transferAccount2 +" FROM appusers WHERE username= '" + username +"';";
+                        mysqlConn.query(query2, function(err, qResult2){
+                            if(err) throw err;
+                            else{
+                            let thisAccount2 = "accountBalance" + transferAccount2;
+                            qResult2[1].forEach(function(account){
+                                accountBalance2 = account[thisAccount2];
+                                //console.log(accountBalance);
+                                if (accountBalance2 < 0){
+                                    console.log("got to accountNotReal");
+                                    
+                                    res.sendFile(__dirname + '/noAccount.html');	
+                                }
+                                else{
+                                    let query3 = "USE users; UPDATE appusers SET accountBalance" + transferAccount1 + " = accountBalance" + transferAccount1 + " - " + amountTransfer + " WHERE username = '" + username + "';";
+                
+                                    let query4 = "USE users; UPDATE appusers SET accountBalance" + transferAccount2 + " = accountBalance" + transferAccount2 + " + " + amountTransfer + " WHERE username = '" + username + "';";
+                                    
+                                    mysqlConn.query(query3, function(err, qResult){
+                                        if(err) throw err;
+                                    
+                                    });
+
+                                    mysqlConn.query(query4, function(err, qResult){
+                                        if(err) throw err;
+                                        
+                                        });
+                                
+                                    res.sendFile(__dirname + '/dashboard.html');
+                                }	
+                            });
+                            }
+                            
+                        });
+                        
+                    }
+                }	
+            });
+            }
+            
+        });
     }
 });
+
 
 app.post('/return', function(req,res){
     res.sendFile(__dirname + '/dashboard.html');
@@ -212,5 +488,6 @@ app.get('/logout', function(req,res){
     req.session.reset();
     res.redirect('/');
 });
+
 
 app.listen(3000);
